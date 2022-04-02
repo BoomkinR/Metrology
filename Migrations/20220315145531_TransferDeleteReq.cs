77d82687520be636021e17be10916c0115ec0a71
Boomkin <alex.renev14@gmail.com>
2022-04-02 07:39:07 +0300

    prebetta

---
+++ Migrations/20220315145531_TransferDeleteReq.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Metrology.Migrations
+{
+    public partial class TransferDeleteReq : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "metrology",
+                table: "users",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "BIRTHDAY", "REGISTRATION_DATE" },
+                values: new object[] { new DateTime(2022, 3, 15, 17, 55, 30, 816, DateTimeKind.Local).AddTicks(9475), new DateTime(2022, 3, 15, 17, 55, 30, 816, DateTimeKind.Local).AddTicks(9483) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "metrology",
+                table: "users",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "BIRTHDAY", "REGISTRATION_DATE" },
+                values: new object[] { new DateTime(2022, 3, 13, 17, 14, 52, 77, DateTimeKind.Local).AddTicks(5554), new DateTime(2022, 3, 13, 17, 14, 52, 77, DateTimeKind.Local).AddTicks(5563) });
+        }
+    }
+}
---
+++ Migrations/20220315150314_TransferDeleteReq2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Metrology.Migrations
+{
+    public partial class TransferDeleteReq2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TRANSFER_DATE",
+                schema: "metrology",
+                table: "transfer_log",
+                type: "TIMESTAMP(0)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP(0)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ACCEPTED_DATE",
+                schema: "metrology",
+                table: "transfer_log",
+                type: "TIMESTAMP(0)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP(0)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "ACCEPTED",
+                schema: "metrology",
+                table: "transfer_log",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.UpdateData(
+                schema: "metrology",
+                table: "users",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "BIRTHDAY", "REGISTRATION_DATE" },
+                values: new object[] { new DateTime(2022, 3, 15, 18, 3, 14, 391, DateTimeKind.Local).AddTicks(3885), new DateTime(2022, 3, 15, 18, 3, 14, 391, DateTimeKind.Local).AddTicks(3894) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TRANSFER_DATE",
+                schema: "metrology",
+                table: "transfer_log",
+                type: "TIMESTAMP(0)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP(0)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ACCEPTED_DATE",
+                schema: "metrology",
+                table: "transfer_log",
+                type: "TIMESTAMP(0)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP(0)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "ACCEPTED",
+                schema: "metrology",
+                table: "transfer_log",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                schema: "metrology",
+                table: "users",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "BIRTHDAY", "REGISTRATION_DATE" },
+                values: new object[] { new DateTime(2022, 3, 15, 17, 55, 30, 816, DateTimeKind.Local).AddTicks(9475), new DateTime(2022, 3, 15, 17, 55, 30, 816, DateTimeKind.Local).AddTicks(9483) });
+        }
+    }
+}
